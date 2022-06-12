79c3069aa9e4f4ff14ca5a17296fe08e65979e75
Afrillian Ramadhan <pingski.ramadhan@gmail.com>
2022-06-12 19:18:48 +0700

    Promise Fullfilled

--- promise.js
+++ promise.js
@@ -1,7 +1,26 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi) => {
+ try {
+  let promise1 = await promiseTheaterIXX();
+  let promise2 = await promiseTheaterVGC();
+  let hasil = 0;
+  promise1.forEach(element => {
+    if(element.hasil == emosi){
+      hasil++;
+    }
+  });
+  promise2.forEach(element => {
+    if(element.hasil == emosi){
+      hasil++;
+    }
+  });
+  return hasil
+ } catch (error) {
+  console.log("Ada error dalam proses pembacaan" + error);
+ }
+}
 
 module.exports = {
   promiseOutput,
